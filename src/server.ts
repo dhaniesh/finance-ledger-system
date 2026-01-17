cf4845a5ecf6975c355150ccc98a65ac989b3502
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-17 23:41:14 +0530

    feat: implement transaction reversal logic

--- src/server.ts
+++ src/server.ts
@@ -31,13 +31,60 @@ app.post("/transactions", async (req: Request, res: Response) => {
             })
         }
         catch (err) {
-            console.error("Error creating transaction:", err);
+            console.error("Error creating transaction, Error:", err);
         }
     }
 })
 
-app.post("/transactions/:id/reverse", (req: Request, res: Response) => {
-    res.json();
+app.post("/transactions/:id/reverse", async (req: Request, res: Response) => {
+    const transactionId = req.params.id;
+    if (!transactionId) {
+        res.status(400).json({ "success": false, error: "Missing transaction id" })
+    }
+    else {
+        try {
+            return await prismaClient.$transaction(async (tx) => {
+                // fetch original transactions with entries
+                const originalTx = await tx.transaction.findUnique({
+                    where: { id: transactionId as string },
+                    include: { entries: true }
+                })
+                if (!originalTx) {
+                    res.status(400).json({ "success": false, error: "Transaction not found" })
+                    return;
+                }
+                if (originalTx?.status === "REVERSED") {
+                    res.status(400).json({ "success": false, error: "Transaction already reversed" })
+                    return;
+                }
+                // create reverse transactions
+                const reveralReference = `reversal_of_${originalTx.id}`
+                const reverseTx = await tx.transaction.create({
+                    data: { reference: reveralReference }
+                })
+                // create reverse ledger entries
+                await tx.ledgerEntry.createMany({
+                    data: originalTx.entries.map((entry) => ({
+                        transactionId: reverseTx.id,
+                        accountId: entry.accountId,
+                        amount: entry.amount.mul(-1)
+                    }))
+                });
+                // mark original transaction as reversed
+                await tx.transaction.update({
+                    where: { id: originalTx.id },
+                    data: { status: "REVERSED" }
+                })
+                return {
+                    originalTransactionId: originalTx.id,
+                    reversalTransactionId: reverseTx.id
+                };
+            })
+        }
+        catch (err) {
+            console.error("Error reversing transaction, Error:", err);
+        }
+    }
 })
 
 app.get("/accounts/:id/balance", (req: Request, res: Response) => {
